740f5b444dbca49948d49263dcd345b50cf04dc1
Hasan SADEBIYIK <naturaturk11@gmail.com>
2022-12-08 17:31:54 +0300

    created task TC002

---
+++ src/testCases/TC002.java
@@ -0,0 +1,16 @@
+package testCases;
+
+public class TC002 {
+
+
+    public static void main(String[] args) {
+
+         // Test case2
+        // writing smoke testing
+        //good poitn he empheseized
+
+
+
+
+    }
+}
